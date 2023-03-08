a2dd83fd1663f2671f71ac275d54f0fda142ac69
skywalker512 <houzhenhong@outlook.com>
2023-03-08 21:03:02 +0800

    fix: clean up some style code

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,10 @@ import TheFooter from '~/components/TheFooter.vue'
 </script>
 
 <template>
-  <main class="font-sans px-4 py-10 text-center text-gray-700 dark:text-gray-200">
-    <RouterView />
-    <TheFooter />
-  </main>
+  <div class="h-screen w-screen font-sans dark:text-white dark:bg-[#121212]">
+    <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
+      <RouterView />
+      <TheFooter />
+    </main>
+  </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 const { iconsPlugin, getIconCollections } = require('@egoist/tailwindcss-icons')
-// const plugin = require('tailwindcss/plugin')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-
-  },
+  darkMode: ['class'],
+  content: ['./src/**/*.{vue,js,ts,jsx,tsx}', './index.html'],
+  theme: {},
   plugins: [
     iconsPlugin({
       // Select the icon collections you want to use
